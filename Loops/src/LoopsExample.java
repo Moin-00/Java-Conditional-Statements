94d6a318da47e41341fe355df735e9a2af224d48
Moin Mulani <moinmulani00@gmail.com>
2023-06-12 08:46:08 +0530

    Adding SwitchCasesExample.java

---
+++ Loops/src/LoopsExample.java
@@ -0,0 +1,33 @@
+public class LoopsExample {
+    public static void main(String[] args) {
+        for(int i =0;i<=10; i++){
+            System.out.println("i ="+ i);
+        }
+        for(int i=10;i>=1;i--){
+            System.out.println("i ="+i);
+        }
+        int i=0;
+        while(i<=10){
+            System.out.println("i="+i);
+            i++;
+        }
+        int j=10;
+        do{
+            System.out.println("j =" + j);
+            j--;
+        }while(j>=1);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
